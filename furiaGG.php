4b5c3025be29a8e48ff7b0b72bdc25c8bc21c4cc
winidevi <winicius.martinz@icloud.com>
2024-10-28 20:32:55 -0300

    Add files via upload

---
+++ furiaGG.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<body> 
+  <!-- Header -->
+
+  <header> 
+        <img src="https://cdn.pandascore.co/images/team/image/128477/600px_furia_esports.png" alt="Logo FURIA" class="logo">
+        
+    </header>
+   
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
+    <style>
+        body {
+            background-color:#fff;
+            color: #000;
+        }
+        header {
+            background-color: #000;
+            color: #fff;
+            padding: 1px;
+            text-align: center;
+
+        }
+        content {
+            padding: 20px;
+            color: #fff;
+         
+
+        }
+        .footer {
+            background-color: #000;
+            color: #fff;
+            text-align: center;
+            padding: 10px;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
+   
+<body>
+
+
+    <div class="content container">
+        <h2> Sobre a Furia </h2>
+        <li>História Fundada em agosto de 2017 em Uberlândia, Minas Gerais, a FURIA foi fruto da idealização do empresário Jaime Pádua, que já planejava investir nos esportes eletrônicos, e dos também empresários André Akkari (jogador de pôquer profissional) e Cris Guedes.<li>
+        
+        <h2>Títulos e conquistas </h2> 
+        <li> Aorus League – Brazil: Finals – Season 1 (2018)</li>
+        <li> ESL LA League Season 1 (2018)</li>
+        <li> Gamers Club Liga Profissional: Abril (2018)</li>
+        <li> GG.BET Ascensão (2018)</li>
+        <li> Aorus League Invitational (2018)</li>
+        <li> Gamers Club Liga Profissional: Maio (2018)</li>
+        <li> ESL Brasil Premier League Season 5 (2018)</li>
+        <li> ESEA Season 30: Premier Division NA (2019)</li>
+        <li> ESEA Season 31: Premier Division NA (2019)</li>
+        <li> ESEA Season 31: Global Challenge (2019)</li>
+        <li> EMF CS:GO World Invitational (2019)</li>
+        <li> Arctic Invitational 2019 (2019)</li>
+        <li> BLAST Premier: Spring 2020 American Showdown (2020)</li>
+        <li> DreamHack Masters Spring NA (2020)</li>
+        <li> DreamHack Open Summer NA (2020)</li>
+        <li> ESL Pro League Season NA (2020)</li>
+        <li>Com os troféus acumulados, a FURIA se consolidou de vez entre os dez melhores times do planeta, chegando a alcançar a sexta colocação no ranking HLTV e a tomar o status que durante muito tempo foi ostentado pela MIBR: melhor equipe do Brasil.</li>
+        
+        
+        <h3>Curiosidades</h3>
+        <p>A FURIA, fundada por Jaime Pádua após uma formação na USP, é uma organização de Esports focada no relacionamento com fãs e jovens talentos. Seu símbolo, a pantera preta, representa luta e resistência, promovendo orgulho entre os fãs. Com a hashtag #DIADEFURIA, a FURIA mobiliza a comunidade nos dias de jogos, criando um clima de Copa do Mundo. Ela já teve sua trajetória registrada no documentário Road to Legends pela RedBull TV, destacando-se entre os 4 melhores do mundo no Counter-Strike.</p>
+
+        <h3>Seja um furioso</h3>
+        <p>Se você é um torcedor apaixonado, considere se tornar um apoiador oficial e nós ajude a trazer mais títulos para o Brasil!!</p>
+    </div>
+   
+    <footer >
+       <p>&copy; <?php echo date("2017"); ?> -todos os direitos reservados. </p>
+     <contet  {
+        text-align: center;
+      }>
+    </footer>
+
+    
+    
+</body>
+</html>
